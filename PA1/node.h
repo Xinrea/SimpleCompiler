8654e869041461a1b00de6aec9f3d15a174e7375
Xinrea <s11s@vip.qq.com>
2018-05-30 14:38:15 +0800

    create AST

--- PA1/node.h
+++ PA1/node.h
@@ -14,6 +14,7 @@ typedef std::vector<NVariableDeclaration*> VariableList;
 class Node {
 public:
     virtual ~Node() {}
+    virtual void printNode(int depth){}
 };
 
 class NExpression : public Node {
@@ -26,18 +27,36 @@ class NInteger : public NExpression {
 public:
     long long value;
     NInteger(long long value) : value(value) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Const-Integer: " << value << std::endl;
+    }
 };
 
 class NDouble : public NExpression {
 public:
     double value;
     NDouble(double value) : value(value) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Const-Float: " << value << std::endl;
+    }
 };
 
 class NIdentifier : public NExpression {
 public:
     std::string name;
     NIdentifier(const std::string& name) : name(name) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Identifier: " << name << std::endl;
+    }
 };
 
 class NMethodCall : public NExpression {
@@ -47,6 +66,16 @@ public:
     NMethodCall(const NIdentifier& id, ExpressionList& arguments) :
         id(id), arguments(arguments) { }
     NMethodCall(const NIdentifier& id) : id(id) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Func_Call: " << std::endl;
+        id.printNode(depth+1);
+        for(auto i : arguments){
+            i->printNode(depth+1);
+        }
+    }
 };
 
 class NBinaryOperator : public NExpression {
@@ -56,6 +85,14 @@ public:
     NExpression& rhs;
     NBinaryOperator(NExpression& lhs, int op, NExpression& rhs) :
         lhs(lhs), rhs(rhs), op(op) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Operator_exp: " << std::endl;
+        lhs.printNode(depth+1);
+        rhs.printNode(depth+1);
+    }
 };
 
 class NAssignment : public NExpression {
@@ -64,12 +101,29 @@ public:
     NExpression& rhs;
     NAssignment(NIdentifier& lhs, NExpression& rhs) : 
         lhs(lhs), rhs(rhs) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Assignment_exp: " << std::endl;
+        lhs.printNode(depth+1);
+        rhs.printNode(depth+1);
+    }
 };
 
 class NBlock : public NExpression {
 public:
     StatementList statements;
     NBlock() { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Block: " << std::endl;
+        for(auto i : statements){
+            i->printNode(depth+1);
+        }
+    }
 };
 
 class NExpressionStatement : public NStatement {
@@ -77,6 +131,13 @@ public:
     NExpression& expression;
     NExpressionStatement(NExpression& expression) : 
         expression(expression) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "Exp: " << std::endl;
+        expression.printNode(depth+1);
+    }
 };
 
 class NVariableDeclaration : public NStatement {
@@ -88,6 +149,15 @@ public:
         type(type), id(id) { }
     NVariableDeclaration(const NIdentifier& type, NIdentifier& id, NExpression *assignmentExpr) :
         type(type), id(id), assignmentExpr(assignmentExpr) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "var_decl: " << std::endl;
+        type.printNode(depth+1);
+        id.printNode(depth+1);
+        if(assignmentExpr)assignmentExpr->printNode(depth+1);
+    }
 };
 
 class NFunctionDeclaration : public NStatement {
@@ -99,4 +169,16 @@ public:
     NFunctionDeclaration(const NIdentifier& type, const NIdentifier& id, 
             const VariableList& arguments, NBlock& block) :
         type(type), id(id), arguments(arguments), block(block) { }
+    virtual void printNode(int depth){
+        for(int i=0;i<depth;++i){
+            std::cout << " ";
+        }
+        std::cout << "fun_decl: " << std::endl;
+        type.printNode(depth+1);
+        id.printNode(depth+1);
+        for(auto i : arguments){
+            i->printNode(depth+1);
+            block.printNode(depth+1);
+        }
+    }
 };
